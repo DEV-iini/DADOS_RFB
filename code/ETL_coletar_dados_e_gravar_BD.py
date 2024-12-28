7d067e29ccd8c5b5cc22fd056c171ffd47aa8afd
Vander Ribeiro Elme <vander@elme.eti.br>
2024-12-27 21:20:13 -0300

    20241227 Atualizacao

--- code/ETL_coletar_dados_e_gravar_BD.py
+++ code/ETL_coletar_dados_e_gravar_BD.py
@@ -25,14 +25,12 @@ import zipfile
 logging.basicConfig(filename='DADOS_RFB.log', level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
-
 def main():
     cnpj_basico = ''
     current = 1
     i = 0
     # Gerar Log
     logging.info(f"Iniciando o processo de carga")
-
 
 def check_diff(url, file_name):
     # Verifica se o arquivo local é idêntico ao arquivo remoto.
@@ -85,7 +83,6 @@ def check_diff(url, file_name):
         print(f"Erro ao acessar o arquivo local: {e}")
         return True
 
-
 def create_dataframe(data, columns):
     """Cria um DataFrame a partir dos dados e colunas fornecidos.
 
@@ -99,7 +96,6 @@ def create_dataframe(data, columns):
     logging.info(f"Criar DataFrame")
     df = dd.DataFrame(data, columns=columns)
     return df
-
 
 def makedirs_custom(path, exist_ok=True, mode=0o755):
     """Cria diretórios recursivamente com controle de existência e permissões.
@@ -121,7 +117,6 @@ def makedirs_custom(path, exist_ok=True, mode=0o755):
         logging.error(f"Erro ao criar diretório {path}: {e}")
         return False
 
-
 def getEnv(env):
     """
     Retorna o valor de uma variável de ambiente.
@@ -134,7 +129,6 @@ def getEnv(env):
     """
     logging.info(f"Retornar valor da variável de ambiente {env}")
     return os.getenv(env)
-
 
 def load_enviroment(local_env):
     """Carrega as variáveis de ambiente de um arquivo .env.
@@ -149,7 +143,6 @@ def load_enviroment(local_env):
     dotenv_path = os.path.join(local_env, '.env')
     load_dotenv(dotenv_path=dotenv_path)
     logging.info(f"Variáveis de ambiente carregadas {dotenv_path}")
-
 
 def define_directories():
     """Define os diretórios de saída e extração dos arquivos.
@@ -172,7 +165,6 @@ def define_directories():
             f"Erro na definição dos diretórios, verifique o arquivo '.env' ou o local informado do seu arquivo de configuração: {e}")
         raise
 
-
 def fetch_data(url):
     """Obtém os dados de uma URL.
 
@@ -191,7 +183,6 @@ def fetch_data(url):
         logging.error(f"Erro ao obter dados da URL {url}: {e}")
         print(f"Erro ao obter dados da URL {url}: {e}")
         raise
-
 
 def extract_files_from_html(html_str, file_extension='.zip'):
     """Extrai os arquivos de uma string HTML.
@@ -220,7 +211,6 @@ def extract_files_from_html(html_str, file_extension='.zip'):
         print(f"Erro ao extrair arquivos da string HTML: {e}")
         raise
 
-
 def delete_files_variable():
     """Deleta a variável Files.
 
@@ -236,7 +226,6 @@ def delete_files_variable():
         print(f"Erro ao deletar a variável Files: {e}")
         return False
 
-
 def print_files_list(files):
     """Imprime a lista de arquivos.
 
@@ -251,14 +240,12 @@ def print_files_list(files):
         logging.error(f"Erro ao imprimir a lista de arquivos: {e}")
         print(f"Erro ao imprimir a lista de arquivos: {e}")
 
-
 def bar_progress(current, total, width=80):
     messagem = "Executando: %d%% [%d / %d] bytes - " % (
         (current * 100)/total, current, total)
     # Don't use print() as it will print in new line every time.
     sys.stdout.write("\r" + messagem)
     sys.stdout.flush()
-
 
 def download_files(Files, base_url, output_files):
     """Baixa os arquivos de uma lista.
@@ -289,7 +276,6 @@ def download_files(Files, base_url, output_files):
         logging.error(f"Erro ao baixar os arquivos: {e}")
         print(f"Erro ao baixar os arquivos: {e}")
 
-
 def extract_files(Files, output_files, extracted_files):
     """Descompacta os arquivos de uma lista.
 
@@ -311,7 +297,6 @@ def extract_files(Files, output_files, extracted_files):
             logging.error(f"Erro ao descompactar os arquivos: {e}")
 
     logging.info(f"Fim descompactando arquivo")
-
 
 def separar_arquivos(items):
     """Separa os arquivos em listas de acordo com o nome.
@@ -363,7 +348,6 @@ def separar_arquivos(items):
         print(f"Erro ao separar os arquivos: {e}")
         raise
 
-
 def connect_to_database(max_retries=3, delay=5):
     """Conecta ao banco de dados com tentativas de reconexão.
 
@@ -398,12 +382,10 @@ def connect_to_database(max_retries=3, delay=5):
             else:
                 raise
 
-
 def ensure_connection(conexao):
     """Garante que a conexão com o banco de dados esteja ativa."""
     if not conexao.is_connected():
         conexao.reconnect(attempts=3, delay=5)
-
 
 def duracao_processo(start_time, end_time):
     """Calcula a duração do processo.
@@ -423,8 +405,7 @@ def duracao_processo(start_time, end_time):
         print(f"Erro ao calcular a duração do processo: {e}")
         raise
 
-
-def process_and_insert_chunk(data, conexao, table_name, table_schema, column_names, batch_size=10000):
+def process_and_insert_chunk(data, conexao, table_name, table_schema, column_names, batch_size=20000):
     """
     Grava dados no banco de dados em lotes.
 
@@ -462,10 +443,8 @@ def process_and_insert_chunk(data, conexao, table_name, table_schema, column_nam
                 cursor.execute(f"SHOW TABLES LIKE '{table_name}'")
                 result = cursor.fetchone()
                 if result is None:
-                    logging.error(f"Falha ao criar a tabela {
-                                  table_name} no banco de dados {current_db}")
-                    raise Exception(f"Falha ao criar a tabela {
-                                    table_name} no banco de dados {current_db}")
+                    logging.error(f"Falha ao criar a tabela {table_name} no banco de dados {current_db}")
+                    raise Exception(f"Falha ao criar a tabela {table_name} no banco de dados {current_db}")
 
             # Processo para inserir os dados no banco
             logging.info(
@@ -518,7 +497,6 @@ def process_and_insert_chunk(data, conexao, table_name, table_schema, column_nam
             logging.info(
                 f"Finalizando processo de inserção de dados na tabela {table_name}")
 
-
 def processar_arquivos(arquivos, extracted_files, conexao, table_name, table_schema, column_names):
     """Processa os arquivos e insere os dados no banco de dados.
 
@@ -547,8 +525,7 @@ def processar_arquivos(arquivos, extracted_files, conexao, table_name, table_sch
                     data.append(tuple(row))
 
                 # Insere o DataFrame no banco de dados
-                process_and_insert_chunk(
-                    data, conexao, table_name, table_schema, column_names)
+                process_and_insert_chunk(data, conexao, table_name, table_schema, column_names)
 
             end_time = time.time()
             duracao_processo(start_time, end_time)
@@ -560,8 +537,7 @@ def processar_arquivos(arquivos, extracted_files, conexao, table_name, table_sch
             cursor.close()
         logging.info(
             f"Finalizando processo de inserção de dados na tabela {table_name}")
-
-
+        return
 def criar_indices(conexao, indices):
     """Cria índices no banco de dados.
 
@@ -584,7 +560,6 @@ def criar_indices(conexao, indices):
         if cursor is not None:
             cursor.close()
 
-
 def listar_arquivos(diretorio):
     """Lista os arquivos de um diretório.
 
@@ -601,7 +576,6 @@ def listar_arquivos(diretorio):
         logging.error(f"Erro ao listar os arquivos: {e}")
         print(f"Erro ao listar os arquivos: {e}")
         raise
-
 
 logging.info(f"Iniciando o processo de carga")
 # Definir diretórios
@@ -650,45 +624,39 @@ else:
                'socios': {'schema': """CREATE TABLE socios (cnpj_basico VARCHAR(8), cnpj_ordem VARCHAR(4), cnpj_dv VARCHAR(2), identificador_socio VARCHAR(1), nome_socio VARCHAR(60), cpf_cnpj_socio VARCHAR(14), qualificacao_socio VARCHAR(2), data_entrada_sociedade VARCHAR(8), pais VARCHAR(5), nome_pais VARCHAR(50), cpf_representante_legal VARCHAR(11), nome_representante_legal VARCHAR(60), qualificacao_representante_legal VARCHAR(2))""", 'columns': ['cnpj_basico', 'cnpj_ordem', 'cnpj_dv', 'identificador_socio', 'nome_socio', 'cpf_cnpj_socio', 'qualificacao_socio', 'data_entrada_sociedade', 'pais', 'nome_pais', 'cpf_representante_legal', 'nome_representante_legal', 'qualificacao_representante_legal']},
                'simples': {'schema': """CREATE TABLE simples (cnpj_basico VARCHAR(8), cnpj_ordem VARCHAR(4), cnpj_dv VARCHAR(2), opcao_simples VARCHAR(1), data_opcao_simples VARCHAR(8), data_exclusao_simples VARCHAR(8), opcao_mei VARCHAR(1), situacao_especial VARCHAR(23), data_situacao_especial VARCHAR(8))""", 'columns': ['cnpj_basico', 'cnpj_ordem', 'cnpj_dv', 'opcao_simples', 'data_opcao_simples', 'data_exclusao_simples', 'opcao_mei', 'situacao_especial', 'data_situacao_especial']},
                'cnae': {'schema': """CREATE TABLE cnae (cnpj_basico VARCHAR(8), cnpj_ordem VARCHAR(4), cnpj_dv VARCHAR(2), cnae_fiscal_principal VARCHAR(7), cnae_fiscal_secundaria VARCHAR(7))""", 'columns': ['cnpj_basico', 'cnpj_ordem', 'cnpj_dv', 'cnae_fiscal_principal', 'cnae_fiscal_secundaria']},
-               }
-       elif 'MUNIC' in item:
-            arquivos['munic'].append(item)
-        elif 'NATJU' in item:
-            arquivos['natju'].append(item)
-        elif 'PAIS' in item:
-            arquivos['pais'].append(item)
-        elif 'QUALS' in item:
-            arquivos['quals'].append(item)
-        # ...definir schemas e colunas para as outras tabelas...
+               'moti': {'schema': """CREATE TABLE moti (cnpj_basico VARCHAR(8), cnpj_ordem VARCHAR(4), cnpj_dv VARCHAR(2), motivo_situacao_cadastral VARCHAR(2), data_situacao_cadastral VARCHAR(8))""", 'columns': ['cnpj_basico', 'cnpj_ordem', 'cnpj_dv', 'motivo_situacao_cadastral', 'data_situacao_cadastral']},
+               'munic': {'schema': """CREATE TABLE munic (codigo_municipio VARCHAR(7), nome_municipio VARCHAR(50), uf VARCHAR(2))""", 'columns': ['codigo_municipio', 'nome_municipio', 'uf']},
+               'natju': {'schema': """CREATE TABLE natju (codigo_natureza_juridica VARCHAR(3), descricao_natureza_juridica VARCHAR(50))""", 'columns': ['codigo_natureza_juridica', 'descricao_natureza_juridica']},   
+               'pais': {'schema': """CREATE TABLE pais (codigo_pais VARCHAR(5), nome_pais VARCHAR(50))""", 'columns': ['codigo_pais', 'nome_pais']},  
+               'quals': {'schema': """CREATE TABLE quals (codigo_qualificacao VARCHAR(2), descricao_qualificacao VARCHAR(50))""", 'columns': ['codigo_qualificacao', 'descricao_qualificacao']}
+               
     }
 
-        for tabela, info in tabelas.items():
-    processar_arquivos(arquivos[tabela], extracted_files,
-                          conexao, tabela, info['schema'], info['columns'])
+for tabela, info in tabelas.items():
+    processar_arquivos(arquivos[tabela], extracted_files, conexao, tabela, info['schema'], info['columns'])
+    logging.info(f"Processo de carga dos arquivos finalizado")
+    insert_end = time.time()
+    Tempo_insert = round(insert_end - insert_start)
+    logging.info(f'Tempo total de execução do processo de carga (em segundos): {Tempo_insert}')
 
-                           logging.info(f"Processo de carga dos arquivos finalizado")
+    index_start = time.time()
 
-                               insert_end = time.time()
-                               Tempo_insert = round(insert_end - insert_start)
-                               print('Tempo total de execução do processo de carga (em segundos): ' + str(Tempo_insert))
-
-                               index_start = time.time()
-
-                               # Criação de índices
-                               indices = [
+    # Criação de índices
+    indices = [
         'CREATE INDEX empresa_cnpj ON empresa(cnpj_basico);',
         'CREATE INDEX estabelecimento_cnpj ON estabelecimento(cnpj_basico);',
         'CREATE INDEX socios_cnpj ON socios(cnpj_basico);',
         'CREATE INDEX simples_cnpj ON simples(cnpj_basico);'
     ]
-        criar_indices(conexao, indices)
+    criar_indices(conexao, indices)
 
-        index_end = time.time()
-        index_time = round(index_end - index_start)
-        print('Tempo para criar os índices (em segundos): ' + str(index_time))
-        # %%
-        print("""Processo 100% finalizado! Você já pode usar seus dados no BD!
+    index_end = time.time()
+    index_time = round(index_end - index_start)
+    logging.info(f'Tempo para criar os índices (em segundos): {index_time}')
+        
+    # %%
+    """Processo 100% finalizado! Você já pode usar seus dados no BD!
      - Desenvolvido por: Aphonso Henrique do Amaral Rafael
      - Adaptado por: Vander Ribeiro Elme
     - Contribua com esse projeto aqui: https://github.com/aphonsoar/Receita_Federal_do_Brasil_-_Dados_Publicos_CNPJ
-    """)
+    """
