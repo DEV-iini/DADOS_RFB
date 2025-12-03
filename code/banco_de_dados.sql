1d43cb84fb622f951238dfe1ddd47f34b6ddcb50
Vander Ribeiro Elme <vander@elme.eti.br>
2025-12-03 16:41:49 -0300

    2025-12-03

--- code/banco_de_dados.sql
+++ code/banco_de_dados.sql
@@ -1,12 +1,6 @@
--- Criar a base de dados "Dados_RFB"
-CREATE DATABASE "Dados_RFB"
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    CONNECTION LIMIT = -1;
+-- Criar a base de dados "Dados_RFB" 
+CREATE DATABASE IF NOT EXISTS Dados_RFB
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
 
-COMMENT ON DATABASE "Dados_RFB"
-    IS 'Base de dados para gravar os dados públicos de CNPJ da Receita Federal do Brasil';
-
--- Diretório físico do banco de dados:
---SHOW data_directory;
+USE Dados_RFB;
